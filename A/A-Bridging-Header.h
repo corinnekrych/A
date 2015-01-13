6de06a8a4dcc60aeb60310f2c79d7c5b577609dc
corinne krych <corinnekrych@gmail.com>
2015-01-13 11:50:34 +0100

    test:

--- A/A-Bridging-Header.h
+++ A/A-Bridging-Header.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <DiffMatchPatch/DiffMatchPatch.h>
 
 //! Project version number for A.
 FOUNDATION_EXPORT double AVersionNumber;
--- File1.swift
+++ File1.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 public class File1 {
-
+ let dmp = DiffMatchPatch()
 }
